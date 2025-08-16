ab47a0a2432eee79eb3e732a8381db9cc5ae1485
Marcos <marcossial043@gmail.com>
2025-08-16 14:24:07 -0300

    Defined UserService and UserController classes

--- src/main/java/com/marcossial/SpringAPI/api/controller/UserController.java
+++ src/main/java/com/marcossial/SpringAPI/api/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.marcossial.SpringAPI.api.controller;
 
+import com.marcossial.SpringAPI.api.model.User;
+import com.marcossial.SpringAPI.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @GetMapping
+    public User getUser(@RequestParam Integer id) {
+        return userService.getUser(id);
+    }
+
 }
--- src/main/java/com/marcossial/SpringAPI/service/UserService.java
+++ src/main/java/com/marcossial/SpringAPI/service/UserService.java
@@ -1,4 +1,31 @@
 package com.marcossial.SpringAPI.service;
 
+import com.marcossial.SpringAPI.api.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class UserService {
+
+    List<User> userList;
+
+    public UserService() {
+        userList = new ArrayList<>();
+
+        User user1 = new User(1, "Ida", 32, "Ida@mail.com");
+
+        userList.addAll(Arrays.asList(user1));
+    }
+
+    public User getUser(Integer id) {
+        for (User user : userList) {
+            if(id == user.getId()) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
